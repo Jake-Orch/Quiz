1ad9c602b5ddabf82f00a0ef4144ea33fcdd7bf1
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-02-21 22:14:09 +0000

    initial js, css and html added

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//create variables for elements i want to create
+//when i click start set the first div display to block, also, set the begin div display to none
+//
+var beginEl = document.getElementById("begin");
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var question4 = document.getElementById("question4");
+var question5 = document.getElementById("question5");
